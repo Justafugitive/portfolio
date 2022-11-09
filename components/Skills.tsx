53c00daf017a2d82e5017b98847e7d8069a13226
Justafugitive <Justafugitive>
2022-11-09 15:40:57 +0200

    correct spelling error

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -21,7 +21,7 @@ const Skills = ({skills}: Props) => {
         </h3>
 
         <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm">
-            Hover over a skill for currency proficiency
+            Hover over a skill for current proficiency
         </h3>
 
         <div className="grid grid-cols-4 gap-5 pt-36 scale-50">
